830790046573d1ff1e2f47d7786d4fcb4b6be54a
FEDso <47750337+FEDso@users.noreply.github.com>
2019-07-31 22:02:58 +0300

    Create non_unique_elements.py

---
+++ Home/non_unique_elements.py
@@ -0,0 +1,44 @@
+"""
+f you have 50 different plug types, appliances wouldn't be available and would be very expensive. But once an electric outlet 
+becomes standardized, many companies can design appliances, and competition ensues, creating variety and better prices for 
+consumers.
+-- Bill Gates
+
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique 
+elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list 
+only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements 
+and result will be [1, 3, 1, 3]. 
+
+Input: A list of integers.
+
+Output: An iterable of integers.
+"""
+
+#Your optional code here
+#You can import some modules or create additional functions
+
+
+def checkio(numbers: list) -> list:
+    numbers.reverse()
+    non_unique_numbers = []
+    for i in range(len(numbers)):
+        n = numbers.pop()
+        if n in non_unique_numbers or n in numbers:
+            non_unique_numbers.append(n)
+    return non_unique_numbers
+
+
+#Some hints
+#You can use list.count(element) method for counting.
+#Create new list with non-unique elements
+#Loop over original list
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
+    
