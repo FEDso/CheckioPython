939727d10077830223da2c60550029fa0f57859b
FEDso <47750337+FEDso@users.noreply.github.com>
2019-08-02 00:26:06 +0300

    Create digits_multiplication.py

---
+++ Elementary/digits_multiplication.py
@@ -0,0 +1,30 @@
+"""
+ You are given a positive integer. Your function should calculate the product of the digits excluding any zeroes.
+
+For example: The number given is 123405. The result will be 1*2*3*4*5=120 (don't forget to exclude zeroes).
+
+Input: A positive integer.
+
+Output: The product of the digits as an integer. 
+"""
+
+
+def checkio(number: int) -> int:
+    p = 1
+    for k in str(number):
+        if k != '0':
+            p *= int(k)
+    return p
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(123405))
+    
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
