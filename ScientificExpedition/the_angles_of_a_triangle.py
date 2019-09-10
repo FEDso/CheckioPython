18ba2536d3020189cd73517560dfa64f92f191c4
FEDso <47750337+FEDso@users.noreply.github.com>
2019-09-10 23:00:04 +0300

    Create the_angles_of_a_triangle.py

---
+++ ScientificExpedition/the_angles_of_a_triangle.py
@@ -0,0 +1,35 @@
+"""
+You are given the lengths for each side on a triangle. You need to find all three angles for this triangle. If the given 
+side lengths cannot form a triangle (or form a degenerated triangle), then you must return all angles as 0 (zero). The 
+angles should be represented as a list of integers in ascending order. Each angle is measured in degrees and rounded to 
+the nearest integer number (Standard mathematical rounding).
+
+Input: The lengths of the sides of a triangle as integers.
+
+Output: Angles of a triangle in degrees as sorted list of integers. 
+"""
+
+from typing import List
+import math
+
+def checkio(a: int, b: int, c: int) -> List[int]:
+    if a + b <= c or a + c <= b or b + c <= a:
+        return [0, 0, 0]
+    cos_a = (b**2 + c**2 - a**2)/(2*b*c)
+    cos_b = (a**2 + c**2 - b**2)/(2*a*c)
+    cos_c = (a**2 + b**2 - c**2)/(2*a*b)
+    alpha = round(math.degrees(math.acos(cos_a)))
+    beta = round(math.degrees(math.acos(cos_b)))
+    gamma = round(math.degrees(math.acos(cos_c)))
+    return sorted([alpha, beta, gamma])
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio(3, 4, 5))
+
+    assert checkio(4, 4, 4) == [60, 60, 60], "All sides are equal"
+    assert checkio(3, 4, 5) == [37, 53, 90], "Egyptian triangle"
+    assert checkio(2, 2, 5) == [0, 0, 0], "It's can not be a triangle"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+    
