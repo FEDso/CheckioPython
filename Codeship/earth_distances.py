b0690ddc97d793d38994b5c9ef540f0b4fd71294
FEDso <47750337+FEDso@users.noreply.github.com>
2020-01-21 00:05:31 +0300

    Update earth_distances.py

--- Codeship/earth_distances.py
+++ Codeship/earth_distances.py
@@ -1,30 +1,81 @@
-from math import pi
+'''
+To describe a specific position on the surface of the Earth, we must rely on the geographic coordinate system. The geographic 
+coordinate system is a method used to give every possible location on Earth to be specified by a set of numbers or letters. A 
+common choice of coordinates is latitude and longitude. With this information we can calculate a distance between two points 
+along a surface.
+
+For simplicity’s sake, we will suppose that the Earth is a perfect sphere with a radius of 6,371 kilometers (it gives a mistake 
+no more than 0.3%). You are given two point coordinates and you must find the shortest distance between these points on the 
+surface of the Earth, measured along the surface of the Earth.
+
+Coordinates are given as a string with the latitude and longitude separated by comma and/or whitespace. Latitude and longitude 
+are represented in the follow format:
+
+    d°m′s″X
+
+
+In this example, "d" is degrees, "m" is minutes, "s" is seconds as integers, while "X" is "N" (north) or "S" (south) for a 
+latitude and "W" (west) or "E" (east) for a longitude.
+
+The result should be given as a number in kilometers with a precision of ±0.1 (100 metres).
+
+Input: Two arguments. Coordinates as strings (unicode).
+
+Output: The distance as a number (int or float).
+
+Example:
+
+1. distance(u"51°28′48″N 0°0′0″E", u"46°12′0″N, 6°9′0″E") == 739.2
+
+2. distance(u"90°0′0″N 0°0′0″E", u"90°0′0″S, 0°0′0″W") == 20015.1
+
+How it is used: The concepts presented in this mission are the exact sorts of concepts used in navigational software, enabling 
+a ship or plane to understand where it is, where it must go and how far it has gone. Along the same vein, Global Positioning 
+Satellites use similar principles to provide pinpoint accurate locations to GPS receivers for use in navigation.
+
+Precondition: Correct Coordinates.
+'''
+
+
+from math import pi, sin, cos, sqrt, atan2
+import re
 R = 6371
-dd = 1852.4*60
-mm = 1852.4
-ss = 1852.4/60
 
 
-def d_to_km(d):
-    return pi*R*d/180
-
-
+def to_coord(d, m, s):
+    L = pi*(float(d) + float(m)/60 + float(s)/3600)/180
+    return L
+    
 def distance(first, second):
     first = first.split(' ')
+    if len(first) == 1:
+        first = first[0].split(',')
     second = second.split(' ')
-    d, m, s = first[0].index('°'), first[0].index('′'), first[0].index('″')
-    xd1 = d_to_km(int(first[0][:d]))
-    xm1, xs1 = 60*d_to_km(int(first[0][d+1:m])) + R, d_to_km(int(first[0][m+1:s]))/3600
-    d, m, s = first[1].index('°'), first[1].index('′'), first[1].index('″')
-    yd1 = d_to_km(int(first[1][:d]))
-    ym1, ys1 = 60*d_to_km(int(first[1][d+1:m])) + R, d_to_km(int(first[1][m+1:s]))/3600
-    d, m, s = second[0].index('°'), second[0].index('′'), second[0].index('″')
-    xd2, xm2, xs2 = d_to_km(dd*int(second[0][:d])), d_to_km(int(second[0][d+1:m]))/60, d_to_km(int(second[0][m+1:s]))/3600
-    d, m, s = second[1].index('°'), second[1].index('′'), second[1].index('″')
-    yd2, ym2, ys2 = d_to_km(dd*int(second[1][:d])), d_to_km(int(second[1][d+1:m]))/60, d_to_km(int(second[1][m+1:s]))/3600
-    result = ((xd1 + xm1 + xs1) - (xd2 + xm2 + xs2))**2 + ((yd1 + ym1 + ys1) - (yd2 + ym2 + ys2))**2
-    print((result/1000)**(0.5))
-    return result**(0.5)
+    if len(second) == 1:
+        second = second[0].split(',')
+    d1, m1, s1, g1 = re.search('(\d+)\D(\d+)\D(\d+)\D(\w+)', first[0]).groups()
+    d2, m2, s2, g2 = re.search('(\d+)\D(\d+)\D(\d+)\D(\w+)', first[1]).groups()
+    x1 = round(to_coord(d1, m1, s1), 6)
+    y1 = round(to_coord(d2, m2, s2), 6)
+    if g1 == 'S':
+        x1 = -x1
+    if g2 == 'W':
+        y1 = -y1
+    d1, m1, s1, g1 = re.search('(\d+)\D(\d+)\D(\d+)\D(\w+)', second[0]).groups()
+    d2, m2, s2, g2 = re.search('(\d+)\D(\d+)\D(\d+)\D(\w+)', second[1]).groups()
+    x2 = round(to_coord(d1, m1, s1), 6)
+    y2 = round(to_coord(d2, m2, s2), 6)
+    if g1 == 'S':
+        x2 = -x2
+    if g2 == 'W':
+        y2 = -y2
+    y = abs(y1 - y2)
+    z1 = (cos(x2)*sin(y))**2 + (cos(x1)*sin(x2) - sin(x1)*cos(x2)*cos(y))**2
+    z2 = sin(x1)*sin(x2) + cos(x1)*cos(x2)*cos(y)
+    result = atan2(sqrt(z1), z2)*R
+    if result == 0.0:
+        return pi*R
+    return result
 
 
 if __name__ == '__main__':
@@ -39,3 +90,4 @@ if __name__ == '__main__':
         distance(u"90°0′0″N 0°0′0″E", u"90°0′0″S, 0°0′0″W"), 20015.1), "From South to North"
     assert almost_equal(
         distance(u"33°51′31″S, 151°12′51″E", u"40°46′22″N 73°59′3″W"), 15990.2), "Opera Night"
+        
