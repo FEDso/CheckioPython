0f3523c808479fc03a16fad2a66e9ac9452db61b
FEDso <47750337+FEDso@users.noreply.github.com>
2020-01-21 23:29:14 +0300

    Create funny_addition.py

---
+++ Ice Base/funny_addition.py
@@ -0,0 +1,29 @@
+"""
+We have an array of two positive integers. Add these two numbers together.
+
+Input: A list of two elements. Each element is a positive integer.
+
+Output: The sum of two numbers.
+
+Example:
+
+  checkio([5, 5]) == 10
+
+  checkio([7, 1]) == 8
+
+  checkio([5, 5]) == 10
+
+How it is used: Just for fun.
+"""
+
+def checkio(data):
+    """The sum of two integer elements"""
+    a, b = data
+    return a + b
+    
+    
+if __name__ == '__main__':
+    assert checkio([5, 5]) == 10, 'First'
+    assert checkio([7, 1]) == 8, 'Second'
+    print('All ok')
+    
